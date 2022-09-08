074a0b43fd396be80863624191e184fba58646f6
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-09-08 11:47:56 +0900

    플레이어 공격 로직 변경

--- Stac/Assets/Scripts/Player/BottleAttack.cs
+++ Stac/Assets/Scripts/Player/BottleAttack.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 public class BottleAttack : MonoBehaviour
 {
-
-
+    Rigidbody2D playerRigid;
     Animator anim;
     bool isAttack;  //공격중임 
     private void Awake()
     {
+        playerRigid = GetComponentInParent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
 
@@ -27,26 +27,39 @@ public class BottleAttack : MonoBehaviour
         }
     }
 
+    private void FixedUpdate()
+    {
+        PlayerMoveCheck();
+    }
+
+    private void PlayerMoveCheck()
+    {
+        transform.localPosition = playerRigid.velocity.x < 0 ?
+            new Vector3(-0.3f, transform.localPosition.y, transform.localPosition.z) : new Vector3(0.3f, transform.localPosition.y, transform.localPosition.z);
+        transform.localScale = playerRigid.velocity.x < 0 ? 
+            new Vector3(-1,transform.localScale.y,transform.localScale.z) : new Vector3(1, transform.localScale.y, transform.localScale.z);
+
+    }
+
     public void Attack()
     {
         StartCoroutine(AttackCoroutine());
     }
 
+
     IEnumerator AttackCoroutine()
     {
         isAttack = true;
-
         anim.SetTrigger("isAttack");
         while (true)
         {
-            
-            if(anim.GetCurrentAnimatorStateInfo(0).IsName("Bottle_Attack")&&
+            if (anim.GetCurrentAnimatorStateInfo(0).IsName("Bottle_Attack") &&
                 anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f) //anim 작동이 다 끝났을 때
             {
                 isAttack = false;
                 yield break;
             }
-                
+
             yield return new WaitForEndOfFrame();
 
         }
